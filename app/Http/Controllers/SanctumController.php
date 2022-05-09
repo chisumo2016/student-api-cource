86ca53f06b2775442e2d04139d511e15eea752f0
bernard chisumo <bchisumo74@yahoo.com>
2022-05-09 07:45:01 +0100

    Sanctum API Authentication Setup Completed

--- app/Http/Controllers/SanctumController.php
+++ app/Http/Controllers/SanctumController.php
@@ -3,19 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Modules\Core\HTTPResponseCodes;
+use App\Modules\Sanctum\SanctumService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Exception;
 
 class SanctumController
 {
+    private  SanctumService $service;
+
+    public  function  __construct(SanctumService $service)
+    {
+        $this->service = $service;
+    }
+
     public  function  issueToken(Request $request) : Response
     {
        //return new Response("Done");
         try {
-               throw new Exception("gg");
-
-
+              $dataArray = ($request->toArray() !== []) ? $request->toArray():$request->json()->all();
+               return new Response(
+                 //RETURN VALUE
+               $this->service->issueToken($dataArray),
+                   HTTPResponseCodes::Success['code']
+               );
+              //throw new Exception("gg");
         }catch (Exception $error){
             return new Response(
                 [
--- database/migrations/2022_05_05_145325_master_seed.php
+++ database/migrations/2022_05_05_145325_master_seed.php
@@ -2,10 +2,25 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    private array $usersSeedValue = [];
+
+    public function __construct()
+    {
+        $this->usersSeedValue = [
+            [
+                "name" => "Emad",
+                "email" => "test@dsm.com",
+                "password" => bcrypt("password"),
+                "created_at" => now()
+            ]
+        ];
+    }
+
     /**
      * Run the migrations.
      *
@@ -13,7 +28,7 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        DB::table("users")->insert($this->usersSeedValue);
     }
 
     /**
@@ -23,6 +38,14 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        DB::table("users")->whereIn(
+            "email",
+            array_map(
+                function ($row) {
+                    return $row["email"];
+                },
+                $this->usersSeedValue
+            )
+        )->delete();
     }
 };
